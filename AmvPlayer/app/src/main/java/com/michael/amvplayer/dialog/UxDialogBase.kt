557f7ff43fd420573f2a236fe25836caf636621f
toyota-m2k <mitsuki.toyota@metamoji.com>
2018-06-22 20:03:57 +0900

    ダイアログクラス作成中

--- AmvPlayer/app/src/main/java/com/michael/amvplayer/dialog/UxDialogBase.kt
+++ AmvPlayer/app/src/main/java/com/michael/amvplayer/dialog/UxDialogBase.kt
@@ -1,4 +1,10 @@
 package com.michael.amvplayer.dialog
 
-class UxDialogBase {
+import android.app.Dialog
+import android.view.View
+
+class UxDialogBase : UxDialog() {
+    override fun createContentView(dlg: Dialog): View {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 }
