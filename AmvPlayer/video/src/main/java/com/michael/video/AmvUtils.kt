9d1d376824ce1b5cb0d70baf59314ffbd1465956
toyota-m2k <mitsuki.toyota@metamoji.com>
2018-08-01 14:23:18 +0900

    時間の書式化を切り出したときにミスが混入

--- AmvPlayer/video/src/main/java/com/michael/video/AmvUtils.kt
+++ AmvPlayer/video/src/main/java/com/michael/video/AmvUtils.kt
@@ -117,14 +117,14 @@ class AmvTimeSpan(private val ms : Long) {
     val hours: Long
         get() = (ms / 1000 / 60 / 60)
 
-    fun formatH() {
-        String.format("%02d:%02d:%02d", hours, minutes, seconds)
+    fun formatH() : String {
+        return String.format("%02d:%02d:%02d", hours, minutes, seconds)
     }
-    fun formatM() {
-        String.format("%02d:%02d", minutes, seconds)
+    fun formatM() : String {
+        return String.format("%02d:%02d", minutes, seconds)
     }
-    fun formatS() {
-        String.format("%02d.%02d", seconds, milliseconds/10)
+    fun formatS() : String {
+        return String.format("%02d.%02d", seconds, milliseconds/10)
     }
 }
 
